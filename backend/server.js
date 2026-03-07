4260c6f116b0b8b725c755f1cebb3d9e23b2021c
Ritisha95 <ritishaagarwal95@gmail.com>
2026-03-07 21:43:46 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+console.log("Starting server...")
+const express = require("express")
+const cors = require("cors")
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// Simple test route
+app.get("/", (req, res) => {
+    res.send("The Centrifuge backend is running")
+})
+
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
